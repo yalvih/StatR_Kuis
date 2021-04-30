8adc8d5b2eff959d859d3ca1fb0b821093a4d85e
yalvih <54298830+yalvih@users.noreply.github.com>
2021-04-30 20:02:50 +0700

    Ini ada perubahan tolong di cek aja

--- Kuis1-SMR-Yalvi.R
+++ Kuis1-SMR-Yalvi.R
@@ -1,1 +1,21 @@
-#test123
+#Nama : Yalvi Hidayat
+#NPM  : 6181801044
+
+#Nama : Michael Alvino W. D.
+#NPM  : 6181801041
+
+##############################
+
+#install library
+library(ggplot2)
+library(datasets)
+library(dplyr)
+library(TeachingDemos)
+library(mlbench)
+
+#dbInit
+data("BostonHousing")
+View(BostonHousing)
+BostonHousing$rad <- as.factor(BostonHousing$rad)
+factor(BostonHousing$rad, levels=c("1","2","3","4","5","6","7","8","24"))
+
