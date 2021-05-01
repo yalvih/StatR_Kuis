8f1ecce6a4dcf7804eb9802badba99436aa33486
yalvih <54298830+yalvih@users.noreply.github.com>
2021-05-01 23:05:39 +0700

    Ada tambahan banyak dan tinggal no 6, no 1 yang kurang sama kata-kata saja

--- Kuis1-SMR-Yalvi.R
+++ Kuis1-SMR-Yalvi.R
@@ -84,20 +84,142 @@ ggplot(boston, aes(x = chas))+
 
 #3
 
-str(BostonHousing)
+str(boston)
+boston$cat_zn <- as.factor(boston$cat_zn)
 
 #interaksi plot
 
-attach(waist)
-interaction.plot(Gender, Sport, Waistline, 
-                 main="Interaction Plot of Waistline by Gender*Sport", 
-                 xlab = "Gender", ylab = "Waistline (cm)",
-                 col=c("red","blue","green"),
-                 lty=c(1,2,3),pch=3,legend = F)
+attach(boston)
+interaction.plot(x.factor = cat_zn, 
+                 trace.factor = chas, 
+                 response = nox, 
+                 main="Interaction Plot of Nitrite Oxide by Proportion Tanah*Sungai Charles", 
+                 xlab = "Proprotion", ylab = "Nox (per 10 mil)",
+                 col=c("red","blue","green"))
 
-legend("topleft",inset = .1, 
-       c("High","Low", "Medium"),
-       col=c("red","blue","green"), 
-       lty=c(1,2,3), pch=3, 
-       title="Sport Intensity")
+interaction.plot(x.factor = rad, 
+                 trace.factor = chas, 
+                 response = nox, 
+                 main="Interaction Plot of Nitrite Oxide by Proportion Tanah*Sungai Charles", 
+                 xlab = "Proprotion", ylab = "Nox (per 10 mil)",
+                 col=c("red","blue","green"))
+
+#4
+
+#uji statistik
+#dketahui 22 untuk niali diujinya. Perlu ada ui statistik t-test
+
+# Hipotesis Nullnya adalaha apakah sama dengan 22.
+# Hipotesis Alt adalah apakah lebih besar dari 22
+
+attach(boston)
+avg_medv <- mean(medv,na.rm = TRUE)
+avg_medv
+
+shapiro.test(medv)
+
+par(mfrow = c(2,2))
+h1 <- hist(medv, 
+           main = "Histogram Rata-Rata Harga Rumah",
+           xlab = "Harga Rata-rata Rumah Median")
+xfit<-seq(min(medv),max(medv),length=40)
+yfit<-dnorm(xfit,mean=mean(medv),sd=sd(medv))
+yfit<-yfit*diff(h1$mids[1:2])*length(medv)
+lines(xfit, yfit, col="red", lwd=2)
+
+# Jika dengan a = 0.05
+t.test(medv,mu=22,alternative = "greater")
+
+# jika dengan a = 0.1
+t.test(medv,mu=22,alternative = "greater",conf.level = 0.9)
+
+# yang alpha 0.05 itu hipotesis nullnya diterima karena nilai p value nya lebih besar dari alphanya (0.09)
+# yang alpha 0.1 itu hipotesis nullnya ditolak karena nilai p valuenya lebih kecil dari alphanya
+
+#b
+data_charles_near <- filter(boston, chas == 1)
+data_charles_no_near <- filter(boston, chas == 0)
+
+#diabaikan
+shapiro.test(data_charles_near$medv)
+shapiro.test(data_charles_no_near$medv)
+
+#cek dahulu variansinya
+var.test(data_charles_near$medv,data_charles_no_near$medv,alternative = "two.sided")
+
+#karena hasil uji variansinya lebih kecil dari alpha, maka variansi kedua populasi itu berbeda
+t.test(data_charles_near$medv,data_charles_no_near$medv)
+
+#Hasilnya ternyata p value juga lebih rendah dari alphanya (0.05). Oleh karena itu H0 ditolak. Rumah yang dekat sungai charles daengan yang tidak dekat memang brbeda harganya
+
+#c
+corrected <- read.delim('corrected.txt')
+
+#lakukan uji normalitas
+shapiro.test(boston$medv)
+shapiro.test(corrected$cmedv)
+
+#asumsikan data sudah berpasangan dan hasilnya pun sama
+var.test(boston$medv,corrected$cmedv,alternative = "two.sided")
+
+#t.test berpasangan
+
+# asumsikan h0 adalah apakah data tersebut sama
+# ha adalah data tersebut berbeda
+
+t.test(boston$medv,corrected$cmedv,alternative = "two.sided",paired = T)
+
+#hasilnya nilai p valuenya lebih besar dari h0 nya yang berarti H0 diterima. Hal ini berarti data tersebut sama saja dan tidak berbeda sama sekali.
+
+#5
+#crim
+ggplot(boston, aes(x=medv, y=crim))+ 
+  geom_point()+
+  xlab("Nilai Median")+
+  ylab("Tingat Kriminalitas")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Tingkat Kriminalitas")
+
+#nox
+ggplot(boston, aes(x=medv, y=nox))+ 
+  geom_point()+
+  xlab("Nilai Median")+
+  ylab("Tingat Kadar Nitrit Oxide")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Tingkat Kadar Nitrit Oxide")
+
+#chas
+ggplot(boston, aes(x=medv, y=chas))+
+  geom_boxplot()+
+  xlab("Nilai Median")+
+  ylab("Jenis Rumah")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Dekatnya Rumah Sungai Charles")
+
+#lstat
+ggplot(boston, aes(x=medv, y=lstat))+
+  geom_point()+
+  xlab("Nilai Median")+
+  ylab("Persentase Kalangan Rendah (dalam persen)")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Persentase Kalangan Rendah")
+
+#b
+ggplot(boston, aes(x=medv, y=b))+
+  geom_point()+
+  xlab("Nilai Median")+
+  ylab("Proporsi Kalangan Ras Kulit Hitam")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Proporsi Kalangan Ras Kulit Hitam")
+
+#tax
+ggplot(boston, aes(x=medv, y=tax))+
+  geom_point()+
+  xlab("Nilai Median")+
+  ylab("Indeks Pajak")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Indeks Pajak")
+
+#rad
+ggplot(boston, aes(x=medv, y=rad))+
+  geom_boxplot()+
+  xlab("Nilai Median")+
+  ylab("Indeks Aksesbilitas ke Jalan Raya")+
+  ggtitle("Scatterplot Hubungan Nilai Median Rumah dengan Indeks Aksesbilitas ke Jalan Raya")
+
+#6
 
